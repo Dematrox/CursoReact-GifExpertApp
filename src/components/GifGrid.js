a32b58192c4530d2c87f66c4120a3d7dd8399e79
Lautaro Dietz <lauta.dietz@gmail.com>
2022-05-27 03:09:03 -0300

    Firs commit

---
+++ src/components/GifGrid.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs';
+import { GifGridItem } from './GifGridItem';
+
+export const GifGrid = ({category}) => {
+
+    const {data:imagen, loading} = useFetchGifs( category );
+  
+    return (
+        <>
+        <h3>{category}</h3>
+        {loading && <p>Cargando...</p>}
+        <div className='card-grid'>
+            {
+                imagen.map(img => (
+                    <GifGridItem 
+                        key={img.id}
+                        {...img}
+                    />
+                ))
+            }            
+        </div>
+        </>
+  )
+}
